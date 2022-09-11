451a1db4fdb88e2d2f714d04924f14b3c9f4886f
Aaditya Semwal <93855836+aadityasemwal7@users.noreply.github.com>
2022-09-11 22:02:59 +0530

    commit @ 11 sep , 10pm

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ app.get("/", (req, res) =>{
 	const url = "https://api.openweathermap.org/data/2.5/weather?appid=62c9718ee0614597f0692e741080504c&q=delhi&units=metric"
 	https.get(url, (response) => {
 		console.log(response.statusCode);
+		response.on('data', (data) => {
+			const weatherData = JSON.parse(data)
+			const temp = weatherData.main.temp
+			const cityName = weatherData.name
+			const countryName = weatherData.sys.country
+			console.log(`City -> ${cityName}, Country -> ${countryName}, Temprature -> ${temp}`);
+		})
 	})
 
 	res.send("server is up and running")
