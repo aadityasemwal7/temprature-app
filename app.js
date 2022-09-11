649776343213aacbf4e6b6375b7b44ee41f059e8
Aaditya Semwal <93855836+aadityasemwal7@users.noreply.github.com>
2022-09-11 18:46:51 +0530

    updated @ 7pm

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express()
 app.get("/", (req, res) =>{
 	const url = "https://api.openweathermap.org/data/2.5/weather?appid=62c9718ee0614597f0692e741080504c&q=delhi&units=metric"
 	https.get(url, (response) => {
-		
+		console.log(response.statusCode);
 	})
 
 	res.send("server is up and running")
