89db9a0d38c56eed1c74d4fade93cb9b4098ce66
Aaditya Semwal <93855836+aadityasemwal7@users.noreply.github.com>
2022-09-11 18:42:47 +0530

    first commit @ 11 sep 6pm

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const https = require("https")
+
+const app = express()
+
+app.get("/", (req, res) =>{
+	const url = "https://api.openweathermap.org/data/2.5/weather?appid=62c9718ee0614597f0692e741080504c&q=delhi&units=metric"
+	https.get(url, (response) => {
+		
+	})
+
+	res.send("server is up and running")
+})
+
+
+
+
+
+
+
+app.listen(3000, () => {
+	console.log('server running on port 3000.');
+})
