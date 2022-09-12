71de3e377dedf3de803c27154d193a1c9b36f4ae
Aaditya Semwal <93855836+aadityasemwal7@users.noreply.github.com>
2022-09-12 16:32:21 +0530

    Pushing @ 12 sep 4pm

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
 const express = require("express")
 const https = require("https")
+const bodyParser = require("body-parser")
 
 const app = express()
 
 app.get("/", (req, res) =>{
-	const url = "https://api.openweathermap.org/data/2.5/weather?appid=62c9718ee0614597f0692e741080504c&q=toronto&units=metric"
-	https.get(url, (response) => {
-		console.log(response.statusCode);
-		response.on('data', (data) => {
-			const weatherData = JSON.parse(data)
-			const temp = weatherData.main.temp
-			const description = weatherData.weather[0].description
-			const icon = weatherData.weather[0].icon
-			const imageUrl = "http://openweathermap.org/img/wn/" + icon  + "@2x.png"
-			const cityName = weatherData.name
-			const countryName = weatherData.sys.country
-			res.write(`<h1 style="font-family: system-ui;">The temprature in ${cityName} is ${temp} degree celcius.</h1>`)	
-			res.write(`<h2 style="font-family: cursive;">Current status is ${description}.</h2>`)
-			res.write(`<img src="${imageUrl}"></img>`)
-			res.send()
-		})
-	})
+	res.sendFile(__dirname + "/index.html")
+	
 
 })
 
+app.post('/', (req, res) => {
+	console.log("Post request recieved!");
+})
 
+// const url = "https://api.openweathermap.org/data/2.5/weather?appid=62c9718ee0614597f0692e741080504c&q=toronto&units=metric"
+// 	https.get(url, (response) => {
+// 		console.log(response.statusCode);
+// 		response.on('data', (data) => {
+// 			const weatherData = JSON.parse(data)
+// 			const temp = weatherData.main.temp
+// 			const description = weatherData.weather[0].description
+// 			const icon = weatherData.weather[0].icon
+// 			const imageUrl = "http://openweathermap.org/img/wn/" + icon  + "@2x.png"
+// 			const cityName = weatherData.name
+// 			const countryName = weatherData.sys.country
+// 			res.write(`<h1 style="font-family: system-ui;">The temprature in ${cityName} is ${temp} degree celcius.</h1>`)	
+// 			res.write(`<h2 style="font-family: cursive;">Current status is ${description}.</h2>`)
+// 			res.write(`<img src="${imageUrl}"></img>`)
+// 			res.send()
+// 		})
+// 	})
 
 
 
@@ -32,4 +38,4 @@ app.get("/", (req, res) =>{
 
 app.listen(3000, () => {
 	console.log('server running on port 3000.');
-})
+}) 
