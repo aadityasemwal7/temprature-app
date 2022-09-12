cb73f3f109e62e361d972d306aeba46c2f6934b9
Aaditya Semwal <93855836+aadityasemwal7@users.noreply.github.com>
2022-09-12 11:12:36 +0530

    New commit

--- app.js
+++ app.js
@@ -4,19 +4,20 @@ const https = require("https")
 const app = express()
 
 app.get("/", (req, res) =>{
-	const url = "https://api.openweathermap.org/data/2.5/weather?appid=62c9718ee0614597f0692e741080504c&q=delhi&units=metric"
+	const url = "https://api.openweathermap.org/data/2.5/weather?appid=62c9718ee0614597f0692e741080504c&q=toronto&units=metric"
 	https.get(url, (response) => {
 		console.log(response.statusCode);
 		response.on('data', (data) => {
 			const weatherData = JSON.parse(data)
 			const temp = weatherData.main.temp
+			const description = weatherData.weather[0].description
 			const cityName = weatherData.name
 			const countryName = weatherData.sys.country
-			console.log(`City -> ${cityName},Country -> ${countryName},Temprature -> ${temp}`);
+			res.write(`<h1>The temprature in ${cityName} is ${temp} degree celcius.</h1>`)	
+			res.write(`<h2>Current status is ${description}.</h2>`)
 		})
 	})
 
-	res.send("server is up and running")
 })
 
 
